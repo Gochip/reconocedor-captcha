7b7e5dd134cbef740b46e1f40ce1f382c29a2e98
Maxi <Maxi@hp-620>
2015-09-29 19:04:07 -0300

    Agrego método generarVector() en Reconocedor

--- src/reconocedor/Reconocedor.java
+++ src/reconocedor/Reconocedor.java
@@ -13,6 +13,15 @@ public class Reconocedor {
      * @param matrices 
      */
     public void entrenar(ArrayList<Matriz> matrices){
+        int[][]m,pesos;
+        for (Matriz matriz : matrices) {
+            m = matriz.getMatriz();
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+                    
+                }
+            }
+        }
         
     }
 
@@ -24,5 +33,20 @@ public class Reconocedor {
      */
     public Matriz reconocer(Matriz matriz){
         return null;
+    }
+    
+    public static int[][] generarVector(int[][] m) {
+        int w = m.length * m[0].length;
+        int[][] resp = new int[1][w];
+        int i = 0;
+        do{
+        for (int j = 0; j < m.length; j++) {
+            for (int k = 0; k < m[j].length; k++) {
+                resp[0][i] = m[j][k];
+                i++;
+            }
+        }
+        }while(i<w);
+        return resp;
     }
 }
