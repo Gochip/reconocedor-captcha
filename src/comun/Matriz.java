3eeab80b100f47f5e51365772732b72ab264a860
Maxi <Maxi@hp-620>
2015-09-30 00:21:50 -0300

    Clase Reconocedor completada e inclusión de los métodos de operaciones con matrices en clase Matriz

--- src/comun/Matriz.java
+++ src/comun/Matriz.java
@@ -18,4 +18,100 @@ public class Matriz {
         hash = 19 * hash + Arrays.deepHashCode(this.matriz);
         return hash;
     }
+    
+    /**
+     * Genera una matriz de 1xn (Vector) a partir de una matriz.
+     * @param m .
+     * @return Retorna una matriz de 1xn.
+     */
+    public int[][] generarVector(int[][] m) {
+        int w = m.length * m[0].length;
+        int[][] resp = new int[1][w];
+        int i = 0;
+        do {
+            for (int j = 0; j < m.length; j++) {
+                for (int k = 0; k < m[j].length; k++) {
+                    resp[0][i] = m[j][k];
+                    i++;
+                }
+            }
+        } while (i < w);
+        return resp;
+    }
+    
+    /**
+     * Genera la transpuesta de una matriz.
+     * @param A.
+     * @return la transpuesta.
+     */
+    public int[][] transpuesta(int[][]A){
+        int[][]resp = new int[A[0].length][A.length];
+        
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                resp[j][i] = A[i][j];
+            }
+        }
+        return resp;
+    }
+    
+    /**
+     * Realiza el producto entre dos matrices A,B.
+     * @param A
+     * @param B
+     * @return Retorna una matriz de nxm.
+     */
+    public int[][] producto(int[][] A, int[][] B) {
+        int result[][] = new int[A.length][B[0].length];
+        
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                for (int k = 0; k < B.length; k++) {
+                    result[i][j] += A[i][k]*B[k][j];
+                }
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Le resta la matriz identidad a una matriz dada.
+     * @param A
+     * @return Retorna con la diagonal principal en cero.
+     */
+    public int[][]restarIdentidad(int[][]A){
+        int I[][] = new int[A.length][A[0].length];
+        for (int i = 0; i < I.length; i++) {
+            for (int j = 0; j < I[i].length; j++) {
+                if(i==j){
+                    I[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = A[i][j] - I[i][j];
+            }
+        }
+        return A;
+    }
+    
+    /**
+     * Función de activación.
+     * @param A
+     * @return Retorna una matriz de 1 y -1.
+     */
+    public int[][] activar(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[i][j] < 0) {
+                    A[i][j] = -1;
+                }
+                if (A[i][j] >= 0) {
+                    A[i][j] = 1;
+                }
+            }
+        }
+        return A;
+    }
 }
--- src/reconocedor/Reconocedor.java
+++ src/reconocedor/Reconocedor.java
@@ -2,18 +2,40 @@ package reconocedor;
 
 import comun.Matriz;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Esta clase reconoce una matriz a partir de un conjunto de matrices de
  * entrenamiento.
  */
 public class Reconocedor {
+    private int[][]pesos;
+    private ArrayList<Integer>hash;
+    
     /**
      * Entrena la red a partir de las matrices.
-     * @param matrices 
+     * @param matrices
      */
     public void entrenar(ArrayList<Matriz> matrices){
+        int h = matrices.get(0).getMatriz().length;
+        int w = matrices.get(0).getMatriz()[0].length;
+        int[][]m,mt;
+        this.pesos = new int[h][w];
         
+        for (Matriz matriz : matrices) {
+            m = matriz.getMatriz();
+            m = matriz.generarVector(m);
+            mt = matriz.transpuesta(m);
+            m = matriz.producto(mt,m);
+            m = matriz.restarIdentidad(m);
+            this.hash.add(Arrays.deepHashCode(matriz.getMatriz()));
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+                    this.pesos[i][j] += m[i][j];
+                }
+            }
+            
+        }
     }
 
     /**
@@ -22,76 +44,20 @@ public class Reconocedor {
      * @return la matriz reconocida. En caso de no reconocer ninguna retorna
      * null.
      */
-    public Matriz reconocer(Matriz matriz){
+    public Matriz reconocer(Matriz matriz) {
+        int[][] m = matriz.getMatriz();
+        m = matriz.generarVector(m);
+        int n = 0;
+        while (Arrays.deepHashCode(m) != this.hash.get(n)) {
+            m = matriz.producto(m, this.pesos);
+            m = matriz.activar(m);
+            for (int hashC : this.hash) {
+                if (Arrays.deepHashCode(m) == this.hash.get(n)) {
+                    return matriz;
+                    }
+            }
+            n++;
+        }
         return null;
-    }
-    
-    /**
-     * Genera una matriz de 1xn (Vector) a partir de una matriz.
-     * @param m Recibe una matriz de enteros contenido en el ArrayList
-     *          que recibe el método entrenar().
-     * @return Retorna una matriz de 1xn.
-     */
-    public int[][] generarVector(int[][] m) {
-        int w = m.length * m[0].length;
-        int[][] resp = new int[1][w];
-        int i = 0;
-        do{
-        for (int j = 0; j < m.length; j++) {
-            for (int k = 0; k < m[j].length; k++) {
-                resp[0][i] = m[j][k];
-                i++;
-            }
-        }
-        }while(i<w);
-        return resp;
-    }
-    
-    /**
-     * Genera la transpuesta a partir de una matriz de 1xn.
-     * @param A Recibe la matriz generada por el método generarVector().
-     * @return Retorna una matriz de nx1.
-     */
-    public static int[][] transpuesta(int[][]A){
-        int[][]resp = new int[A[0].length][A.length];
-        
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[i].length; j++) {
-                resp[j][i] = A[i][j];
-            }
-        }
-        
-        return resp;
-    }
-    
-    /**
-     * Realiza el producto entre dos matrices A,B y le resta 
-     * la matriz identidad I.
-     * @param A
-     * @param B
-     * @return Retorna una matriz de nxm.
-     */
-    public static int[][] producto(int[][] A, int[][] B) {
-        int suma = 0;
-        int result[][] = new int[A.length][B[0].length];
-        int I[][] = new int[A.length][B[0].length];
-
-        
-        for (int i = 0; i < I.length; i++) {
-            for (int j = 0; j < I[i].length; j++) {
-                if(i==j){
-                    I[i][j] = 1;
-                }
-            }
-        }
-        
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B[0].length; j++) {
-                for (int k = 0; k < B.length; k++) {
-                    result[i][j] += (A[i][k]*B[k][j])-I[i][j];
-                }
-            }
-        }
-        return result;
     }
 }
